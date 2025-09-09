8be864930d5026943ca4138e44482f8b8e5bd5bf
MoisesTafarello <moisesgabrieltafarello5@gmail.com>
2025-09-09 12:46:57 -0300

    Fazendo o Menu do Usuário

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from classes import *
+from funcoes import *
+
+def menu ():
+    locadora = Locadora ()
+
+while True:
+    try:
+        os.system ("cls")
+        print ("Sistema de Locadora")
+        print ("1 - Cadastrar Cliente")
+        print ("2 - ")
